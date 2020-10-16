a8dd0b4fd24e3a4ed595e23fc588104d66eeadc4
lazaropd <lazaro.domiciano@gmail.com>
2020-10-16 18:17:45 -0300

    version 0.1 - deploying test app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# msc
+# My Smart Clinic Plugin
+
+This is an open plugin for My Smart Clinic app.
+
+It was created in order to test the new Share and automatic deploy in Streamlit 0.69.
+
+For more information, please contact lazaro.domiciano@gmail.com
---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+st.title('My Smart Clinic')
+
+
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+
+expander = st.beta_expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
+
+
+
+
+
+
+
+
+
+
