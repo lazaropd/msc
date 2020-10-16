793be9518b125ef8e574c2f22e6c88cd9106eafb
lazaropd <lazaro.domiciano@gmail.com>
2020-10-16 18:31:15 -0300

    v0.1 - improvements to try a shortened url

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,18 @@ expander.write("Here you could put in some really, really long explanations...")
 
 
 
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+
+
 
 
 
